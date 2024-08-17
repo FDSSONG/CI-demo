5ba9d3975ee2cc9f4b7d039fe6450e9f1c0dab79
爱吃喵酱的花椒 <150331560+FDSSONG@users.noreply.github.com>
2024-08-17 21:13:29 +0800

    8.17

--- java_CI/src/main/java/org/example/Main.java
+++ java_CI/src/main/java/org/example/Main.java
@@ -1,6 +1,13 @@
 package org.example;
 
 public class Main {
+
+
+    public void testAdd(){
+        int addsum = 4+2;
+        assert addsum == 6;
+        System.out.println("持续集成第一次测试");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
