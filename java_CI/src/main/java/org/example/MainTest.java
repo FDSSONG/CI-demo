51bfffe53a8e6900a0a6c9388f80d1704453f99a
爱吃喵酱的花椒 <150331560+FDSSONG@users.noreply.github.com>
2024-08-17 21:38:42 +0800

    1

--- java_CI/src/main/java/org/example/MainTest.java
+++ java_CI/src/main/java/org/example/MainTest.java
@@ -10,17 +10,23 @@ public class MainTest {
 
     @Before
     public void setUp() throws Exception {
+        System.out.println("第一次测试开始");
     }
 
     @After
     public void tearDown() throws Exception {
+        System.out.println("第一次测试结束");
     }
 
     @Test
     public void testAdd() {
+        int addsum = 4+2;
+        assert addsum == 6;
+        System.out.println("持续集成第一次测试");
     }
 
     @Test
     public void main() {
+        System.out.println("第一次测试");
     }
 }
